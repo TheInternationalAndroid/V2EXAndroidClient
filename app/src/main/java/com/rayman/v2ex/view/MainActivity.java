7ab83be6c59625baed2ab0df068242c59cc28dce
LenaYan <slyyantao@gmail.com>
2016-01-18 22:33:54 +0800

    Add tools

--- app/src/main/java/com/rayman/v2ex/view/MainActivity.java
+++ app/src/main/java/com/rayman/v2ex/view/MainActivity.java
@@ -3,15 +3,15 @@ package com.rayman.v2ex.view;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.rayman.v2ex.R;
+import com.rayman.v2ex.view.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

c9cb452d2dccaf89ef8b25d89596f8a1994ddc37
Rayman.t.Yan <tao.yan@istuary.com>
2016-11-28 12:36:55 +0800

    Move templates to lib

--- Templates/templates_activity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-activityTmpDir="V2EXActivityTmp"
-DIR="/Applications/Android Studio.app/Contents/plugins/android/lib/templates/activities/${activityTmpDir}/"
-if [ -d "$DIR" ]; then
-    printf '%s\n' "Template is exist,deleting it---->($DIR)"
-    rm -rf "$DIR"
-fi
-    cp -r "${activityTmpDir}" "$DIR"
-
--- Templates/templates_frag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-fragTmpFirName="V2EXFragmentTmp"
-DIR="/Applications/Android Studio.app/Contents/plugins/android/lib/templates/other/${fragTmpFirName}/"
-if [ -d "$DIR" ]; then
-    printf '%s\n' "Template is exist,deleting it---->($DIR)"
-    rm -rf "$DIR"
-fi
-    cp -r "$fragTmpFirName"/ "$DIR"
-
